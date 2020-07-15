95eff0c64f9f586ee140ef022c8c5baa3a8702d8
MacBook Air <macbookair@White-Fang-Mac.local>
2020-07-15 11:53:13 +0300

    15.07.20 11:53

--- venv/regression_linear.py
+++ venv/regression_linear.py
@@ -4,70 +4,62 @@ from reader import Reader
 from sklearn.linear_model import LinearRegression
 
 
-def get_price_data():
-    """Метод получения данных по ценам за весь период."""
-    x = Reader('USD000000TOD_1M_131001_131231.txt')
-    data = x.read().splitlines()
-    open_price = [data[1].split(',')[4]]
-    close_column = [x.split(',')[7] for x in data][1:]
-    combined = open_price + close_column
-    result = [float(item) for item in combined]
-    return result
+class Linear_regression:
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.x = []
+        self.y = []
+        self.r_sq = 0.0
+        self.result = []
+
+    def x_preparation(self):
+        """Для получения x в методе для обучения с учителем """
+        for i in range(len(self.dataset)):
+            if i + 10 < len(self.dataset):
+                self.x += [self.dataset[i: i + 10]]
+        return self.x
+
+    def y_preparation(self):
+        """Для получения y в методе для обучения с учителем """
+        for i in dataset[10:]:
+            self.y += [i]
+        return self.y
+
+    def calculate(self):
+        """Метод подсчета данных для линейной регрессии."""
+        x = self.x_preparation()
+        y = self.y_preparation()
+        model = LinearRegression().fit(x, y)
+        self.r_sq = model.score(x, y)
+        self.result = model.predict(x)
+        return self.r_sq, self.result
+
+    @staticmethod
+    def get_price_data():
+        """Метод получения данных по ценам за весь период."""
+        x = Reader('USD000000TOD_1M_131001_131231.txt')
+        data = x.read().splitlines()
+        open_price = [data[1].split(',')[4]]
+        close_column = [x.split(',')[7] for x in data][1:]
+        combined = open_price + close_column
+        result = [float(item) for item in combined]
+        return result
 
 
-dataset = get_price_data()
+"""Выделение данных и получения данных линейной регрессии"""
 
-x = list()
-for i in range(len(dataset)):
-    if i + 10 < len(dataset):
-        x += [dataset[i: i + 10]]
-y = list()
-for i in dataset[10:]:
-    y += [i]
-
-model = LinearRegression().fit(x, y)
-r_sq = model.score(x, y)
-
-result = model.predict(x)
-
-print('predicted response:', result, sep='\n')
-print('coefficient of determination:', r_sq)
-print('intercept:', model.intercept_)
-print('slope:', model.coef_)
-
-# print(x)
-# print(y)
-# print(len(price))
-# print(len(time))
-# print(price)
-# print(time)
+dataset = Linear_regression.get_price_data()
+new = Linear_regression(dataset)
+r_sq, result = new.calculate()
+print("Coefficient of determination = ", r_sq)
 
 """Вывод на графики полученных данных"""
 
 fig, ax = plt.subplots()
-ax.text(10000, 10000, 'Coefficient of determination = 0.998696594488011')
 ax.plot(dataset, label='Исходные данные')
 ax.plot(result, label='Линейная регрессия')
 ax.set_xlabel('Время (мин)')
 ax.set_ylabel('Цена (руб)')
 ax.legend()
 plt.show()
-
-# class Linear_regression:
-#
-#     def __call__(self, values, window=1):
-#         """Метод подсчёта Simple moving average с помощью свертки и окна"."""
-#         weights = np.repeat(1.0, window) / window
-#         smas = np.convolve(values, weights, 'valid')
-#         return smas
-#
-#     @staticmethod
-#     def get_price_data():
-#         """Метод получения данных по ценам за весь период."""
-#         x = Reader('USD000000TOD_1M_131001_131231.txt')
-#         data = x.read().splitlines()
-#         open_price = [data[1].split(',')[4]]
-#         close_column = [x.split(',')[7] for x in data][1:]
-#         combined = open_price + close_column
-#         result = [float(item) for item in combined]
-#         return result
